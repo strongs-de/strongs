6e27415b1029052c969e432c13c9d1f709c2fae5
Mirko Hecky <mirhec@nochains.de>
2014-03-25 11:00:47 +0100

    Beschreibung hinzugefügt (über Tooltips)   => https://github.com/strongs-de/strongs/issues/20

--- strongs/grammar_parser.py
+++ strongs/grammar_parser.py
@@ -40,12 +40,12 @@ def get_grammar_name(short):
 
 		# Mood
 		ret += '<br/><b>Modus:</b> '
-		if short[4+add] == 'I': ret += 'Indikativ'
+		if short[4+add] == 'I': ret += get_indikativ()
 		elif short[4+add] == 'S': ret += 'Sub-/Konjunktiv'
 		elif short[4+add] == 'O': ret += 'Optativ'
-		elif short[4+add] == 'M': ret += 'Imperativ'
-		elif short[4+add] == 'N': ret += 'Infinitiv'
-		elif short[4+add] == 'P': ret += 'Partizip'
+		elif short[4+add] == 'M': ret += get_imperativ()
+		elif short[4+add] == 'N': ret += get_infinitiv()
+		elif short[4+add] == 'P': ret += get_partizip()
 		elif short[4+add] == 'R': ret += 'Imperativer Partizip'
 
 		# Person
@@ -65,5 +65,30 @@ def get_grammar_name(short):
 
 
 def get_aorist():
-	return '<abbr title="Von Wikipedia ..." rel="tooltip">Aorist</abbr>'
+	return u'<abbr title="<b><u>Aorist (aus Wikipedia):</b></u><br/>\
+		Der Aorist ist in einigen indogermanischen Sprachen ein Tempus der Vergangenheit. \
+		Im Gegensatz zu anderen Vergangenheitstempora wie beispielsweise dem Imperfekt oder \
+		dem Perfekt beschreibt er Vorgänge in der Vergangenheit, die als individuelle einmalig \
+		abgeschlossene Handlungen, also punktuell, betrachtet werden. Er beinhaltet damit den \
+		perfektiven Verbalaspekt. Diese Aspektbedeutung des Aorist kann in einigen Formen die \
+		zeitliche verdrängen." rel="tooltip">Aorist</abbr>'
+	# return '<abbr class="tip" data-tip="Ein Aorist ist ...">Aorist</abbr>'
 
+def get_indikativ():
+	return u'<abbr title="<b><u>Indikativ (aus Wikipedia):</b></u><br/>Der Indikativ wird für die Darstellung der Wirklichkeit benutzt. Er ist sozusagen der Normalmodus in allen Texten. Der Indikativ steht für ein tatsächliches Geschehen. Er existiert im Gegensatz zu den beiden anderen Modi in allen menschlichen Sprachen und ist der Modus, der im Deutschen am meisten verwendet wird." rel="tooltip">Indikativ</abbr>'
+
+
+def get_partizip():
+	return u'<abbr title="<b><u>Partizip (aus Wikipedia):</b></u><br/>Partizip (lat. participium, von particeps „teilhabend“; Plural: Partizipien) ist eine infinite Verbform. Die Bezeichnung deutet auf die Teilhabe (Partizipation) an den Eigenschaften sowohl von Adjektiven als auch von Verben hin. Ähnliches bringt die deutsche Bezeichnung Mittelwort zum Ausdruck, weil das Partizip gleichsam in der Mitte zwischen Verb und Adjektiv steht. Als infinite Verbform ist seine Form unabhängig von grammatikalischen Verbkategorien (Zahl, Person und Modus). Beispiel: die liebende Mutter" rel="tooltip">Partizip</abbr>'
+
+
+def get_imperativ():
+	return u'<abbr title="<b><u>Imperativ (aus Wikipedia):</b></u><br/>Der Imperativ (von latenisch imperare: befehlen) ist ein Modus des Verbs. Er wird in erster Linie für Aufforderungen und Befehle oder Ratschläge und Einladungen benutzt." rel="tooltip">Imperativ</abbrv>'
+
+
+def get_infinitiv():
+	return u'<abbr title="<b><u>Infinitiv (aus Wikipedia):</b></u><br/>Infinitiv (lat. infinitum, „das Unbestimmte“, „das Unvollendete“) ist der Name für eine Verbform, in der Numerus und (normalerweise) Person nicht ausgedrückt werden. Infinitivformen gibt es gleichwohl in verschiedenen Tempora („gesehen haben“) und unterschiedlicher Diathese („gesehen worden sein“). Zusammen mit den Partizipien und dem Inflektiv gehört der Infinitiv zu den infiniten Verbformen. Im Deutschen und in vielen anderen Sprachen wird der Infinitiv als Zitierform eines Verbs verwendet; dies ist jedoch nicht in allen Sprachen so." rel="tooltip">Infinitiv</abbrv>'
+
+
+# def get__():
+	# return u'<abbr title="<b><u>Von Wikipedia:</b></u><br/></abbrv>'
