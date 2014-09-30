c7fedb4ddda2dc5345c390e16bb8c96f4816fd7f
Mirko Hecky <mirhec@nochains.de>
2014-09-30 19:38:59 +0200

    Removed PSQL database ... we still go with a sqlite database.

--- myproject/settings.py
+++ myproject/settings.py
@@ -64,12 +64,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'psql': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'strongs_live',
-        'USER': 'strongs_live_admin',
-        'PASSWORD': 'FX3Hrhv3QwUpgRpUfmm'
     }
 }
 
