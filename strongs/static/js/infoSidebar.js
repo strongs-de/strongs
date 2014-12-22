4297c74e45dd59fda39db1c216fe5b1e772f746e
Mirko Hecky <mirhec@nochains.de>
2014-12-22 20:17:09 +0100

    Design enhancements especially for mobile devices. Hopefully quite correct now (may not be all done but should be a good step to go).

--- strongs/static/js/infoSidebar.js
+++ strongs/static/js/infoSidebar.js
@@ -5,9 +5,8 @@ function animateOpenInfobox() {
     setTimeout(function() {
         var button = $('#sb-menu');
         if (!$(button).hasClass('sb-menu-open')) {
-//                $('#sb-menu').animate({'right': $('#sb-menu').outerWidth()}, 300);
-            $('#sb-slidebox').animate({'right': $('#sb-slidebox').outerWidth()}, 300);
             $('#sb-menu').addClass('sb-menu-open');
+            $('#sb-slidebox').addClass('sb-menu-open');
         }
     }, 25);
 }
@@ -17,9 +16,9 @@ function animateCloseInfobox() {
     setTimeout(function() {
         var button = $('#sb-menu');
         if ($(button).hasClass('sb-menu-open')) {
-//                $('#sb-menu').stop(true).animate({'right': '0px'}, 100);
-            $('#sb-slidebox').stop(true).animate({'right': '0px'}, 100);
             $(button).removeClass('sb-menu-open');
+            $('#sb-slidebox').removeClass('sb-menu-open');
+
             // remove anchor link
             parent.location.hash = '';
         }
@@ -31,7 +30,7 @@ function loadStrong( strong, vers, word ) {
     xhReq.open("GET", "/strong/" + strong + "/" + vers + "/" + word, false);
     xhReq.send(null);
     var serverResponse = xhReq.responseText;
-    $('#sb-menu > #info-content').html(serverResponse);
+    $('#sb-menu > #info-content').html(serverResponse + '<br><br><br>');
     initTooltips();
     document.location = document.location.href.match(/(^[^#]*)/)[0] + '#' + strong + "/" + vers + "/" + word;
 }
@@ -44,6 +43,66 @@ function strongclick(ev) {
 }
 
 function initStrongLinks() {
+
+    // $('.sb-strong').each(function(idx, ele) {
+    //     // add data-ajax attribute
+    //     $(ele).attr('data-ajax', $(ele).attr('data-strong') + '/' + $(ele).parent().attr('id') + '/' + $(ele).text())
+    // });
+
+    // // JBOX
+    // $('.sb-strong').jBox('Modal', {
+    //     ajax: {
+    //         // url: '/strong/' + $(this).attr('data-strong') + '/' + $(this).parent().attr('id') + '/' + $(this).text(),
+    //         // url: '/strong/5457/Joh1_8/anfang',
+    //         url: '/strong/',
+    //         getData: 'data-ajax',
+    //         type: 'POST'
+    //     }
+    // });
+
+    // TIPPED
+    // Tipped.create('.sb-strong', function() {
+    //     // return "<strong>" + $(this).data('content') + "<\/strong>"
+    //     $.ajax({
+    //         type: 'GET',
+    //         url: '/strong/' + $(this).attr('data-strong') + '/' + $(this).parent().attr('id') + '/' + $(this).text(),
+    //         success: function(data) {
+    //             // update our tooltip content with our returned data and cache it
+    //             // origin.tooltipster('content', data).data('ajax', 'cached');
+    //             this.update(data);
+    //         }
+    //     });
+    // });
+
+    // TOOLTIPSTER
+    // $('.sb-strong').tooltipster({
+    //     content: 'Loading...',
+    //     contentAsHTML: true,
+    //     interactive: true,
+    //     onlyOne: true,
+    //     trigger: 'click',
+    //     positionTracker: true,
+    //     updateAnimation: false,
+    //     functionBefore: function(origin, continueTooltip) {
+    //
+    //         // we'll make this function asynchronous and allow the tooltip to go ahead and show the loading notification while fetching our data
+    //         continueTooltip();
+    //
+    //         // next, we want to check if our data has already been cached
+    //         if (origin.data('ajax') !== 'cached') {
+    //             $.ajax({
+    //                 type: 'GET',
+    //                 url: '/strong/' + $(this).attr('data-strong') + '/' + $(this).parent().attr('id') + '/' + $(this).text(),
+    //                 success: function(data) {
+    //                     // update our tooltip content with our returned data and cache it
+    //                     origin.tooltipster('content', data).data('ajax', 'cached');
+    //                 }
+    //             });
+    //         }
+    //     }
+    // });
+
+
     $('.sb-strong').on('click', strongclick);
 
     // open and close the searchsidebar
